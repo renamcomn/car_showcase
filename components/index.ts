519c7542ff18beb760435943d190d038d6b07769
renamcomn <renamcomn@outlook.com>
2023-07-04 23:26:53 -0300

    refactor components and add CarDetails and ShowMore

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,19 @@
-import Hero from "./Hero";
-import CustomButton from "./CustomButton";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
-import SearchBar from "./SearchBar";
-import CustomFilter from "./CustomFilter";
-import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import ShowMore from "./ShowMore";
+import Hero from "./Hero";
+import SearchBar from "./SearchBar";
+import NavBar from "./NavBar";
 
 export {
-    Hero,
-    CustomButton,
-    NavBar,
-    Footer,
-    SearchBar,
-    CustomFilter,
-    SearchManufacturer,
-    CarCard
-}
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
